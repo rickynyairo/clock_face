0ad7a145d4d6c8aef7dc543b8a38a4f71b533360
Ricky Nyairo <rickynyairo@gmail.com>
2020-03-06 10:40:26 +0300

    adds clock face angle calculator and tests

---
+++ clock_face.py
@@ -0,0 +1,23 @@
+"""
+module to calculate the angle between clock arms
+given the time
+"""
+# import typing
+
+
+class Clock:
+    """class to hold the attributes of clock class
+    """
+
+    def __init__(self, hours=0, minutes=0):
+        self.hours = hours
+        self.minutes = minutes
+
+    def calculate_angle(self):
+        """
+        calculate the angle between the hour and minute hand
+        """
+        hour_angle = (self.hours + self.minutes / 60) * (360 / 12)
+        minute_angle = (self.minutes / 60) * 360
+        angle = abs(hour_angle - minute_angle)
+        return angle
---
+++ clock_face_test.py
@@ -0,0 +1,25 @@
+import unittest
+from clock_face import Clock
+
+class TestClockFace(unittest.TestCase):
+
+    def setUp(self):
+        self.clock = Clock()
+
+    def test_calculate_angle(self):
+        self.clock.hours = 9
+        self.clock.minutes = 30
+        self.assertEqual(self.clock.calculate_angle(), 105.0)
+    
+    def test_calculate_midnight(self):
+        self.clock.hours = 0
+        self.clock.minutes = 0
+        self.assertEqual(self.clock.calculate_angle(), 0.0)
+
+    def test_calculate_3_40(self):
+        self.clock.hours = 3
+        self.clock.minutes = 40
+        self.assertEqual(self.clock.calculate_angle(), 130.0)
+
+if __name__ == '__main__':
+    unittest.main()
