2093a1bf34136171a2ba5b8c81e494de09c6bd9b
Ricky Nyairo <rickynyairo@gmail.com>
2020-03-11 15:35:58 +0300

    adds 24hr functionality

--- clock_face.py
+++ clock_face.py
@@ -2,8 +2,6 @@
 module to calculate the angle between clock arms
 given the time
 """
-# import typing
-
 
 class Clock:
     """class to hold the attributes of clock class
@@ -17,7 +15,7 @@ class Clock:
         """
         calculate the angle between the hour and minute hand
         """
-        hour_angle = (self.hours + self.minutes / 60) * (360 / 12)
+        hour_angle = (self.hours % 12 + self.minutes / 60) * (360 / 12)
         minute_angle = (self.minutes / 60) * 360
         angle = abs(hour_angle - minute_angle)
         return angle
--- clock_face_test.py
+++ clock_face_test.py
@@ -10,14 +10,24 @@ class TestClockFace(unittest.TestCase):
         self.clock.hours = 9
         self.clock.minutes = 30
         self.assertEqual(self.clock.calculate_angle(), 105.0)
-    
+
     def test_calculate_midnight(self):
+        self.clock.hours = 12
+        self.clock.minutes = 0
+        self.assertEqual(self.clock.calculate_angle(), 0.0)
+
+        # 24hr midnight:
         self.clock.hours = 0
         self.clock.minutes = 0
         self.assertEqual(self.clock.calculate_angle(), 0.0)
 
     def test_calculate_3_40(self):
         self.clock.hours = 3
+        self.clock.minutes = 40
+        self.assertEqual(self.clock.calculate_angle(), 130.0)
+
+    def test_calculate_15_40(self):
+        self.clock.hours = 15
         self.clock.minutes = 40
         self.assertEqual(self.clock.calculate_angle(), 130.0)
 
